d9be0c2664c50c0d65851dac4f13d7168ec47812
jiyinyiyong <jiyinyiyong@gmail.com>
2016-10-11 17:52:52 +0800

    refactor to get transition

--- src/global_popup/comp/popup_stack.cljs
+++ src/global_popup/comp/popup_stack.cljs
@@ -3,23 +3,74 @@
   (:require [respo.alias :refer [create-comp div]]
             [respo-ui.style :as ui]
             [hsl.core :refer [hsl]]
-            [global-popup.comp.popover :refer [comp-popover]]
-            [global-popup.comp.modal :refer [comp-modal]]))
+            [global-popup.style :as style]))
+
+(def style-zero-inside
+ (merge
+   style/transition
+   {:background-color (hsl 0 0 100),
+    :width 0,
+    :opacity 0,
+    :padding 0,
+    :height 0}))
+
+(defn on-backdrop [e dispatch!] (dispatch! :popup/drop nil))
+
+(defn render-modal [popup render-popup]
+  (div
+    {:style
+     (merge
+       style/transition
+       style/style-modal
+       {:width (.-innerWidth js/window),
+        :height (.-innerHeight js/window)}),
+     :event {:click on-backdrop}}
+    (div
+      {:style (merge style/transition style/style-box),
+       :event {:click (fn [e dispatch!])}}
+      (render-popup popup))))
+
+(defn render-popover [popup render-popup]
+  (div
+    {:style
+     (merge
+       style/transition
+       style/style-popover
+       {:top (:y (:position popup)), :left (:x (:position popup))})}
+    (div {:style style-zero-inside})
+    (render-popup popup)))
+
+(def style-zero
+ (merge
+   ui/center
+   {:transform "scale(0)",
+    :top 0,
+    :width 0,
+    :opacity 0,
+    :position "fixed",
+    :height 0,
+    :left 0}))
 
 (defn render [popups render-popup]
   (fn [state mutate!]
     (div
       {}
-      (->>
-        popups
-        (map
-          (fn [popup] [(:id popup)
-                       (case
-                         (:type popup)
-                         :popover
-                         (comp-popover popup render-popup)
-                         :modal
-                         (comp-modal popup render-popup)
-                         nil)]))))))
+      (conj
+        (->>
+          popups
+          (map-indexed
+            (fn [idx popup] [idx
+                             (case
+                               (:type popup)
+                               :popover
+                               (render-popover popup render-popup)
+                               :modal
+                               (render-modal popup render-popup)
+                               nil)]))
+          (into []))
+        [(count popups)
+         (div
+           {:style (merge style/transition style-zero)}
+           (div {:style style-zero-inside}))]))))
 
 (def comp-popup-stack (create-comp :popup-stack render))
---
+++ src/global_popup/style.cljs
@@ -0,0 +1,34 @@
+
+(ns global-popup.style
+  (:require [hsl.core :refer [hsl]] [respo-ui.style :as ui]))
+
+(def style-box
+ {:background-color (hsl 0 0 100),
+  :width 480,
+  :opacity 1,
+  :padding 16,
+  :height 160})
+
+(def style-popover
+ (merge
+   ui/center
+   {:box-shadow (str "1px 1px 8px " (hsl 0 0 10 0.4)),
+    :transform "scale(1)",
+    :background-color (hsl 0 0 100 0.9),
+    :width 120,
+    :position "fixed",
+    :border-radius "2px",
+    :height 40}))
+
+(def style-modal
+ (merge
+   ui/fullscreen
+   ui/center
+   {:transform "scale(1)",
+    :background-color (hsl 0 0 0 0.3),
+    :width (.-innerWidth js/window),
+    :height (.-innerHeight js/window)}))
+
+(def transition
+ {:transition-timing-function "ease-in-out",
+  :transition-duration "400ms"})
